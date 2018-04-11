6eb5747ceaa0385147b7c2f1ee0e87e8f019bb89
patchen2 <patchen2@cisco.com>
2018-04-10 17:08:28 -0700

    first commit

--- app/Router.js
+++ app/Router.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Index from './pages/Index';
+import Album from './pages/Album';
 
 export default class Root extends React.Component {
     render() {
         return (
             <Router>
                 <Switch>
-
+                    <Route path="/album" component={Album}/>
                     <Route path="/" component={Index}/>
                 </Switch>
             </Router>
